d087417926d5c9bf71b87cc20dbfcc5340e866ff
Saiki <quodlunofficial@gmail.com>
2026-01-30 16:03:54 +0800

    Add Welcome Greeting

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
-fn main () {}
+fn main ()
+{
+    let length_input: &str;
+
+    print! ( "Welcome To Crystear.\nPassword Length: " );
+
+    length_input = "20";
+
+    print! ( "Length Set To: {}\n", length_input );
+}
