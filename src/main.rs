db14f25f568662ecd2da81e06dbfebcb6e4400f2
Saiki <115924991+Quodlun@users.noreply.github.com>
2026-01-31 00:15:00 +0800

    Add Main Page Mode Select

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
 use text_io::read;
+fn mode_select () -> i32
+{
+    println! ( "1. Generate Password" );
+    println! ( "2. Settings" );
+    println! ( "3. Exit" );
+
+    let mode: i32 = read! ();
+
+    return mode;
+}
+
+fn mode_switch ( mode: i32 )
+{
+    match mode
+    {
+        1 => print!("\x1B[2J"),
+        2 => println! ( "Opening Settings..." ),
+        3 => println! ( "Exiting..." ),
+        _ => println! ( "Invalid Option Selected." ),
+    }
+}
+
+/*
+fn length_setting () -> i32
+{
+    
+
+    print! ( "Password Length: ");
+    let password_length: i32 = read! ();
+
+    return password_length;
+}
+*/
+
+
 
 fn main ()
 {
     println! ( "Welcome To Crystear." );
-    
-    print! ( "Password Length: ");
-    let password_length: i32 = read! ();
 
-    print! ( "Length Set To: {}\n", password_length );
+    mode_switch ( mode_select () );
+    
+
+    // let password_length: i32 = length_setting ();
+
+    // print! ( "Length Set To: {}\n", password_length );
 }
