5e2cb78ba9f720d840d7648f1c96d554cc0c9c34
Saiki <115924991+Quodlun@users.noreply.github.com>
2026-01-31 03:32:20 +0800

    Add Character Setting

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,85 @@
 use text_io::read;
-fn mode_select () -> i32
+
+struct Characters
 {
-    println! ( "1. Generate Password" );
-    println! ( "2. Settings" );
-    println! ( "3. Exit" );
-
-    let mode: i32 = read! ();
-
-    return mode;
+    uppercase: bool,
+    lowercase: bool,
+    numbers: bool,
+    symbols: bool,
 }
-
-fn mode_switch ( mode: i32 )
-{
-    match mode
-    {
-        1 => print!("\x1B[2J"),
-        2 => println! ( "Opening Settings..." ),
-        3 => println! ( "Exiting..." ),
-        _ => println! ( "Invalid Option Selected." ),
-    }
-}
-
-/*
-fn length_setting () -> i32
-{
-    
-
-    print! ( "Password Length: ");
-    let password_length: i32 = read! ();
-
-    return password_length;
-}
-*/
-
-
 
 fn main ()
 {
-    println! ( "Welcome To Crystear." );
+    let mut characters_settings: Characters = Characters
+    {
+        uppercase: true,
+        lowercase: true,
+        numbers: true,
+        symbols: true,
+    };
 
-    mode_switch ( mode_select () );
-    
-
-    // let password_length: i32 = length_setting ();
-
-    // print! ( "Length Set To: {}\n", password_length );
+    main_page_mode_select ( &mut characters_settings );
 }
+
+fn main_page_mode_select ( characters_settings: &mut Characters )
+{
+    loop
+    {
+        print!("\x1B[2J");
+        println! ( "Welcome To Crystear." );
+        println! ( "1. Generate Password" );
+        println! ( "2. Settings" );
+        println! ( "3. Exit" );
+
+        match read! ()
+        {
+            1 => print!("\x1B[2J"),
+            2 => setting_mode_select ( characters_settings ),
+            3 => std::process::exit(0),
+            _ => ()
+        }
+    }
+}
+
+fn setting_mode_select ( characters_settings: &mut Characters )
+{
+    loop
+    {
+        print!("\x1B[2J");
+        println! ( "Settings Menu:" );
+        println! ( "1. Using Characters" );
+        println! ( "2. Back" );
+
+        match read! ()
+        {
+            1 => using_characters ( characters_settings ),
+            2 => main_page_mode_select ( characters_settings ),
+            _ => ()
+        }
+    }
+}
+
+fn using_characters ( characters_settings: &mut Characters )
+{
+    loop
+    {
+        print!("\x1B[2J");
+        println! ( "Character Settings: (Select Numbers To Change " );
+        println! ( "1. Use Uppercase Letters: {}", characters_settings.uppercase );
+        println! ( "2. Use Lowercase Letters: {}", characters_settings.lowercase );
+        println! ( "3. Use Numbers: {}", characters_settings.numbers );
+        println! ( "4. Use Symbols: {}", characters_settings.symbols );
+        println! ( "5. Back" );
+
+        match  read! ()
+        {
+            1 => characters_settings.uppercase = !characters_settings.uppercase,
+            2 => characters_settings.lowercase = !characters_settings.lowercase,
+            3 => characters_settings.numbers = !characters_settings.numbers,
+            4 => characters_settings.symbols = !characters_settings.symbols,
+            5 => { setting_mode_select ( characters_settings ); break; },
+            _ => (),
+        }
+    }
+}
+
