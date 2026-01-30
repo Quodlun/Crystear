42175bc59e129c4de27f1dab00cb2fc2f1029949
Saiki <quodlunofficial@gmail.com>
2026-01-30 16:02:23 +0800

    Main Branch Init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-// use std::io;
-
-fn main ()
-{
-    let length_input: &str;
-
-    print! ( "Welcome To Crystear.\nPassword Length: " );
-
-    length_input = "20";
-
-    print! ( "Length Set To: {}\n", length_input );
-}
+fn main () {}
