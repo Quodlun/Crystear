3c718cbef928813cb3c34f76704bd7373acd722b
Saiki <quodlunofficial@gmail.com>
2026-02-03 14:35:24 +0800

    Move Code To Library

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,6 @@
 use rand::prelude::*;
 use text_io::read;
-
-const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
-const NUMBERS: &str = "0123456789";
-const SYMBOLS: &str = "!@#$%^&*()-_=+[]{}|;:',.<>?/`~";
-
-enum CharType {
-    Uppercase,
-    Lowercase,
-    Numbers,
-    Symbols,
-}
-
-struct Characters {
-    uppercase: bool,
-    lowercase: bool,
-    numbers: bool,
-    symbols: bool,
-}
-
-impl Characters {
-    pub fn toggle(&mut self, t: CharType) {
-        let is_last_one = self.active_count_check();
-
-        let field = match t {
-            CharType::Uppercase => &mut self.uppercase,
-            CharType::Lowercase => &mut self.lowercase,
-            CharType::Numbers => &mut self.numbers,
-            CharType::Symbols => &mut self.symbols,
-        };
-
-        if !*field || !is_last_one {
-            *field = !*field;
-        }
-    }
-
-    fn active_count_check(&self) -> bool {
-        let active_count = [self.uppercase, self.lowercase, self.numbers, self.symbols]
-            .iter()
-            .filter(|&&active| active)
-            .count();
-
-        active_count == 1
-    }
-
-    pub fn get_pool(&self) -> String {
-        let mut pool = String::new();
-
-        if self.uppercase {
-            pool.push_str(UPPERCASE);
-        }
-
-        if self.lowercase {
-            pool.push_str(LOWERCASE);
-        }
-
-        if self.numbers {
-            pool.push_str(NUMBERS);
-        }
-        if self.symbols {
-            pool.push_str(SYMBOLS);
-        }
-
-        pool
-    }
-}
+use crystear_core::{CharType, Characters};
 
 fn main() {
     let mut characters_settings: Characters = Characters {
@@ -218,7 +153,7 @@ fn length_check(input: &str) -> Result<usize, String> {
         .map_err(|_| String::from("Please enter a valid number."))?;
 
     if length == 0 || length > 100 {
-        return Err(String::from("Length must be between 0 and 100."));
+        return Err(String::from("Length must be between 1 and 100."));
     }
 
     Ok(length)
